0ed9e21391b3b4af8623be39f89f179321368ca5
Mikeeer <my email>
2022-12-05 19:21:01 +0300

    Example03

---
+++ Example03/Program.cs
@@ -0,0 +1,28 @@
+﻿int GetNamber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.Write(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Введите число больше 0.");
+        }
+    }
+
+    return result;
+}
+
+int namber = GetNamber("Введите число: ");
+
+for (int i = 1; i <= namber; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}, ");
+}
